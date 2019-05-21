fbd2d9f87ff16f7c32fc4f8a22c64377b3bfa255
Chiara Ripanti <chiararipanti@MacBook-Air-di-Chiara.local>
2019-05-21 21:31:35 +0100

    Setting un env variables

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const publicFolderPath = path.join(__dirname, "../public")
 const partialsFolderPath = path.join(__dirname, '../templates/partials')
 const app = express()
 
+const port = process.ENV.port || 3000
+
 //Setup handleBars engine and views location 
 app.set('view engine', 'hbs')
 app.set('views', 'templates/views')
@@ -90,6 +92,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("server is running")
 })
